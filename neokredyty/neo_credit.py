094496415dbc7991287a6700375fb2de5699d345
patryk353 <patryk.czorniej@viait.pl>
2014-08-14 13:09:11 +0200

    Działający harmonogram

--- neokredyty/neo_credit.py
+++ neokredyty/neo_credit.py
@@ -14,7 +14,7 @@ class neo_credit(Model):
     _columns = {
         'name' : fields.char('Nazwa',required=True),
         'schedule_ids' : fields.one2many('neo.schedule','credit_id'),
-        'client_id': fields.many2one('res.partner', 'Klient',required=True),
+        'client_id': fields.many2one('res.partner', 'Klient', required=True),
         'telephone' : fields.integer('Nr kontaktowy'),
         'address' : fields.char('Adres korespondencyjny'),
         'email' : fields.char('Mail'),
@@ -33,13 +33,13 @@ class neo_credit(Model):
         'report_krd_big' : fields.integer('Raport KRD/BIG'),
         'termination' : fields.selection((('1','TAK'),('2','NIE')), 'Wypowiedzenie umowy'),
         'charge_call' : fields.float('Opłata za wezwanie'),
-        'installment' : fields.float('Rata',readonly=True),
+        'installment' : fields.float('Rata', readonly=True),
         'total_arrears' : fields.float('Suma zaległości'),
         'amount_insurance' : fields.float('Kwota ubezpieczenia'),
         'amount_credit' : fields.float('Kwota kredytu'),
-        'total_liabilities' : fields.float('Suma zobowiązania',readonly=True),
+        'total_liabilities' : fields.float('Suma zobowiązania', readonly=True),
         'stage_id' : fields.many2one('neo.credit.stage','Status'),
-        'type_schedule' : fields.selection((('1','Raty malejące'),('2','Raty równe')), 'Raty'),
+        'type_credit' : fields.selection((('1','Raty malejące'),('2','Raty równe')), 'Raty'),
     }
     
     
@@ -61,14 +61,14 @@ class neo_credit(Model):
         
         vals_schedule = {}
         vals_schedule['credit_id'] = credit_id
+        
         rata = total_liabilities / credit.period
-        rata2 = total_liabilities*(q**credit.period)*(q-1)/((q**credit.period)-1) 
         q=1+((0.0833333333)*(credit.interest/100))
-        
-        
+        rata2 = total_liabilities*(q**credit.period)*(q-1)/((q**credit.period)-1) 
+         
         i=0
-        pdb.set_trace()
-        if credit.type_schedule == '1':
+        
+        if credit.type_credit == '1':
             while i < credit.period:
                 odsetki = dosplaty * credit.interest/100/12
                 dosplaty = dosplaty - rata
@@ -79,18 +79,24 @@ class neo_credit(Model):
                 vals_schedule['name'] = i+1
                 schedule_obj.create(cr, uid, vals_schedule)
                 i += 1
-        elif credit.type_schedule == '2':
+                vals_schedule['installment'] = rata
+                self.write(cr, uid, [credit_id], vals_schedule)
+        elif credit.type_credit == '2':
             while i < credit.period:
+
                 odsetki = (q-1)*dosplaty
                 kapital = 0.0
-                kapital = rata - odsetki
+                kapital = rata2 - odsetki
                 dosplaty = dosplaty - kapital
                 vals_schedule['repayment_interest'] = odsetki
                 vals_schedule['repayment_credit'] = kapital
                 vals_schedule['repay'] = dosplaty
-                vals_schedule['full_installment'] = rata
+                vals_schedule['full_installment'] = rata2
                 vals_schedule['name'] = i+1
                 schedule_obj.create(cr, uid, vals_schedule)
+                vals_schedule['installment'] = rata2
+                self.write(cr, uid, [credit_id], vals_schedule)
                 i += 1
+            
         return credit_id
     
